96f7e352966ff1847b3bb97b394571b8378da156
Sascha Diercks <sascha.diercks@googlemail.com>
2016-06-21 21:20:33 +0200

    MOD: clock-prototype

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,23 @@
+<?php
+	// Set timezone and get current time
+	date_default_timezone_set('Europe/Berlin');
+	$currentTime = array(
+		'hours' => date(g),
+		'minutes' => date(i),
+		'seconds' => date(s)
+	);
+
+	// set degrees for timevalues to position handles
+	$degreeHours = 360/12;
+	$degreeMinutes = 360/60;
+	$degreeSeconds = 360/60;
+
+	// set handles to display current time
+	$handHours = $degreeHours * $currentTime[hours];
+	$handMinutes = $degreeMinutes * $currentTime[minutes];
+	$handSeconds = $degreeSeconds * $currentTime[seconds];
+?>
+
 <!DOCTYPE html>
 <html dir="ltr" lang="de">
 <head>
@@ -7,6 +27,39 @@
 	<meta name="keywords" content="keywords" />
 	<?php require_once 'modules/framework/head-meta.php'; ?>
 
+	<style media="screen">
+	.hand {
+		background-color: #000;
+		position: absolute;
+		transform-origin: 50% 0;
+		top: 50%;
+		left: 50%;
+	}
+	#hand-hours {
+		height: 120px;
+		width: 10px;
+		<?php echo 'transform: rotate('.$handHours.'deg) translateY(-100%)'; ?>
+	}
+	#hand-minutes {
+		height: 240px;
+		width: 10px;
+		<?php echo 'transform: rotate('.$handMinutes.'deg) translateY(-100%)'; ?>
+	}
+	#hand-seconds {
+		height: 240px;
+		width: 2px;
+		<?php echo 'transform: rotate('.$handSeconds.'deg) translateY(-100%)'; ?>
+	}
+
+	#clockface {
+		border: 1px solid #ccc;
+		border-radius: 99em;
+		margin: auto;
+		position: relative;
+		height: 500px;
+		width: 500px;
+	}
+	</style>
 </head>
 
 <body id="index">
@@ -16,6 +69,23 @@
 		<div class="inner">
 
 			<main role="content">
+				<h1>Clock</h1>
+
+
+				<div id="clock-analog">
+					<div id="clockface">
+						<div class="hand" id="hand-hours"></div>
+						<div class="hand" id="hand-minutes"></div>
+						<div class="hand" id="hand-seconds"></div>
+					</div>
+				</div>
+
+				<div id="clock-digital">
+					<h2>Aktuelle Uhrzeit</h2>
+					<?php
+						echo $currentTime[hours].':'.$currentTime[minutes].':'.$currentTime[seconds];
+					?>
+				</div>
 			</main>
 		</div>
 
--- htdocs/modules/framework/head-meta.php
+++ htdocs/modules/framework/head-meta.php
@@ -8,8 +8,8 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
 
 <!-- Favicon, CSS -->
-<link href="favicon.ico" rel="shortcut icon"/>
+<!-- <link href="favicon.ico" rel="shortcut icon"/>
 <link href="apple-touch-icon-foto-57x57-precomposed.png" rel="apple-touch-icon-precomposed" />
 <link href="apple-touch-icon-foto-114x114-precomposed.png" rel="apple-touch-icon-precomposed" sizes="114x114" />
 <link href="/assets/css/mobile.min.css" rel="stylesheet" type="text/css" media="screen and (max-width:767px)" />
-<link href="/assets/css/site.min.css" rel="stylesheet" type="text/css" media="screen and (min-width:768px)" />
+<link href="/assets/css/site.min.css" rel="stylesheet" type="text/css" media="screen and (min-width:768px)" /> -->
